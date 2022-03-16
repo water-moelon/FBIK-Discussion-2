97d18ace9b2f9e0cc3d5e6944eb15ec13b64c10b
FBIK <83176414+BovineBeta@users.noreply.github.com>
2022-03-16 12:03:49 +0800

    Add files via upload

---
+++ JSDiscussion.js
@@ -0,0 +1,15 @@
+/*
+ *仓库地址:https://github.com/FBIK-Discussion/FBIK-Discussion
+ ***************************************************
+ *申请加入的方式
+ *1.Fork本仓库.
+ *2.修改这个文件, 按照字母顺序添加自己账号信息的键值对.格式如下:
+ *
+ * 你的昵称: ["你的Github Username", "你的邮箱"]
+ *
+ *3.提交Pull requests.
+ *4.若pr成功,请查看你的Github账号所绑定的邮箱并同意team邀请.
+*/
+var jsd_member = {
+    example: ["@example", "example@example.com"]
+};
